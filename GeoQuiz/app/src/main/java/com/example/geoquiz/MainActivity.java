3f471389df754152f6c6615b935d5d0eebcf7397
DustinTylerMorin <dustintmorin82@aol.com>
2020-02-10 23:19:11 -0500

    Updated GeoQuiz application to fix a bug where the application would forget current question index when the device was rotated. Fixed this by storing the value of current index and passing that value into onCreate.

--- GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.java
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -3,17 +3,19 @@ package com.example.geoquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 
 public class MainActivity extends AppCompatActivity {
     private TextView mQuestionText;
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private static final String TAG = "GeoQuiz";
+    private static final String KEY_INDEX = "index";
     private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_ocean, true),
             new Question(R.string.question_africa, false),
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
             new Question(R.string.question_asia, true),
             new Question(R.string.question_australia, false),
     };
+
     private int mCurrentIndex = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +40,9 @@ public class MainActivity extends AppCompatActivity {
                 boolean mAnswerBool = mQuestionBank[mCurrentIndex].isAnswerTrue();
                 String mAnswer = Boolean.toString(mAnswerBool);
                 if (mAnswerBool == true)
-                    Toast.makeText(MainActivity.this, "You are correct, the answer was "+ mAnswer +".", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "You are correct, the answer was " + mAnswer + ".", Toast.LENGTH_SHORT).show();
                 else
-                    Toast.makeText(MainActivity.this, "You are incorrect, the answer was "+ mAnswer +".", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "You are incorrect, the answer was " + mAnswer + ".", Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -47,23 +50,44 @@ public class MainActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick (View v){
+            public void onClick(View v) {
                 boolean mAnswerBool = mQuestionBank[mCurrentIndex].isAnswerTrue();
                 String mAnswer = Boolean.toString(mAnswerBool);
                 if (mAnswerBool == false)
-                    Toast.makeText(MainActivity.this, "You are correct, the answer was "+ mAnswer +".", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "You are correct, the answer was " + mAnswer + ".", Toast.LENGTH_SHORT).show();
                 else
-                    Toast.makeText(MainActivity.this, "You are incorrect, the answer was "+ mAnswer +".", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "You are incorrect, the answer was " + mAnswer + ".", Toast.LENGTH_SHORT).show();
             }
         });
         mNextButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick (View v){
+            public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                int question = mQuestionBank[mCurrentIndex].getTextResId();
-                mQuestionText.setText(question);
+                updateQuestion();
             }
         });
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
+        updateQuestion();
     }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionText.setText(question);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+
+    }
+
+
+
 }
+
